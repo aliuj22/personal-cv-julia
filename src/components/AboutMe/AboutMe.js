000d48d700a7d6a74fea956c69ab12b48672fe6d
Julia Wos <juliawos22@gmail.com>
2022-03-01 18:47:04 +0100

    about me update

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -6,19 +6,19 @@ const AboutMe = () => {
     <div id="about">
       <h2>ABOUT ME</h2>
       <p>
-        I'm a 22 year old, based in my dream city, Stockholm since 2018.
-        Originally I'm from Poland. Currently studying my (not so long ago
-        found) passion, Frontend Development at Hyper Island. I'm curious about
-        the world and it's history, love reading books, which I buy more, than
-        can read. Passionate about people and making positive changes to the
-        world. I have experience in team leading, working in fast paced
-        environment and under stressful conditions, due to having worked as an
-        Assistant Manager in a popular restaurant. I enjoy working in teams as
-        well as individually. I have skills in problem solving, organising and
-        logistics. I speak fluent Polish and English, as well as Swedish on a
-        basic, everyday level. During my time in Hyper Island I've been learning
-        and gaining experience in html, css, javascript, ux and ui design,
-        react, figma, GitHub and agile methods of working.
+        I'm a 22 year old, based in Stockholm since 2018. Originally I'm from
+        Poland. Currently studying my (not so long ago found) passion, Frontend
+        Development at Hyper Island. I'm curious about the world and it's
+        history, love reading books, which I buy more, than can read. Passionate
+        about people and making positive changes to the world. I have experience
+        in team leading, working in fast paced environment and under stressful
+        conditions, due to having worked as an Assistant Manager in a popular
+        restaurant. I enjoy working in teams as well as individually. I have
+        skills in problem solving, organising and logistics. I speak fluent
+        Polish and English, as well as Swedish on a basic, everyday level.
+        During my time in Hyper Island I've been learning and gaining experience
+        in html, css, javascript, ux and ui design, react, figma, GitHub and
+        agile methods of working.
       </p>
     </div>
   );
