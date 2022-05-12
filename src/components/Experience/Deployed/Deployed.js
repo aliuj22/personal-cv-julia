8bfe8a4ab5527d5c5dccf9cce7a390692677bcc1
Julia Wos <juliawos22@gmail.com>
2022-05-12 15:01:49 +0200

    fixed grid in projects

--- src/components/Experience/Deployed/Deployed.js
+++ src/components/Experience/Deployed/Deployed.js
@@ -2,20 +2,22 @@ import React from 'react';
 
 const Deployed = (props) => {
   return (
-    <div className="card">
-      <ul id="projects-container">
-        {props.description.map((item, index) => (
-          <li key={index}>
-            <hr />
-            <h3>{item.name}</h3> <p>{item.description} </p> <p>{item.tech}</p>
+    <div id="projects-container" className="card">
+      {props.description.map((item, index) => (
+        <div className="cardFlex" key={index}>
+          <div className="gridLeft">
+            <h3>{item.name}</h3> <p>{item.description} </p>{' '}
+          </div>
+          <div className="gridRight">
+            <p>{item.tech}</p>
             {item.link2 && item.link2}
             {item.gif && item.gif}
             <a target="_blank" href={item.link} rel="noreferrer">
               <em>Check it out here!</em>
             </a>
-          </li>
-        ))}
-      </ul>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
