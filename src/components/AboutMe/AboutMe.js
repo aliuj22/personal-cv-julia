95594493f0608b4497bb0c185d6b4535d5bc8b0a
Julia Wos <juliawos22@gmail.com>
2023-01-29 19:39:24 +0100

    about me text update for 2023, nav spacing fix

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -5,21 +5,26 @@ const AboutMe = () => {
   return (
     <div id="about">
       <p id="aboutText">
-        I'm a 22 year old, based in Stockholm since 2018. Originally I'm from
-        Poland 🇵🇱. Currently studying my recent passion, Frontend Development at
-        Hyper Island. I'm curious about the world and it's history, love reading
-        books 📖, which I buy more, than can read. Passionate about people and
-        making positive changes to the world. I have experience in team leading,
-        working in fast paced environment and under stressful conditions, due to
-        having worked as an Assistant Manager in a popular restaurant. I enjoy
-        working in teams as well as individually. I have skills in problem
-        solving, organising and logistics. <br /> I speak fluent
+        I am a motivated Fullstack Developer soon-to-be graduate from Hyper
+        Island, eager to apply my skills and gain professional experience as a
+        developer. <br />
+        My experience as a Fullstack Development Intern at 12iD has allowed me
+        to develop and implement new features and design on a demo website
+        showcasing the 12iD mobile app. During my time there, I successfully
+        completed a full redesign, implemented new flows and features, and
+        gained a deep understanding of the product. I also worked on fixing bugs
+        and adding new features to an in-production admin portal. <br /> My
+        previous role as Assistant Manager at Greasy Spoon has honed my skills
+        in problem-solving, team leadership, logistics and conflict resolution.
+        I enjoy working in teams as well as individually. <br /> I speak fluent
         <b> Polish</b> and <b>English</b>, as well as Swedish on a basic,
-        everyday level. <br /> During my time in Hyper Island I've been learning
-        and gaining experience in <b>HTML</b>, <b>CSS</b>, <b> JavaScript</b>,
-        <b> UX</b> and <b>UI</b> design, <b>ReactJS</b>, <b>NextJS</b>,{' '}
-        <b>Figma</b>, <b>git</b> and <b>GitHub</b>, <b>agile</b> methods of working in teams and basics of <b>MERN</b> stack, <b>RESTful APIs</b>,{' '}
-        and <b>TDD</b>.
+        everyday level. <br /> During my time in Hyper Island and 12iD I've been
+        learning and gaining experience in <b>HTML</b>, <b>CSS</b>,{' '}
+        <b> JavaScript</b> and
+        <b> TypeScript</b>,<b> UX</b> and <b>UI</b> design, <b>ReactJS</b>,{' '}
+        <b>NextJS</b>, <b>Figma</b>, <b>git</b> and <b>GitHub</b>, <b>agile</b>{' '}
+        methods of working in teams, <b>MERN</b> stack, <b>RESTful APIs</b>, and{' '}
+        <b>TDD</b>.
       </p>
     </div>
   );
